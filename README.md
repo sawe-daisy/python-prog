381d7c1f4a31ea4e018a02604abc4b41f3432d54
sawe-daisy <deesawe16@gmail.com>
2024-01-11 16:31:48 +0300

    python programme to modify list

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # python-prog
+
+### Description
+
+Welcome aboard fellow developer, This is a Python scripts which you are free to clone and run.The script takes in a list of integer values seperated by comma.
+    -  accepts a list of integers
+    -  emits an error message if the list is not a multiple of 10 in length
+    - returns or prints a list of integers based on the input list, but with items at positions which are a multiple of 2 or 3 removed
+
+### Instructions
+- Make sure python is installed in your machine
+- Clone this github repository
+- open the project and run checkList.py
+- You will be prompted to enter list of numbers, do so with comma separeted values.
+- Your modified list
---
+++ checkList.py
@@ -0,0 +1,19 @@
+def checkList(inputlist):
+    # check if length is divisible by 10
+    if len(inputlist) % 10 != 0:
+        print("Error : the length of the list is not a multiple of 10")
+        return
+        # filter list to remove items in position of multiples of 2 and 3
+    res_list = [item for index, item in enumerate(inputlist) if index % 2 != 0 and index % 3 != 0]
+    print("List after modification : ", res_list)
+
+
+if __name__ == "__main__":
+    try:
+        values = input("Enter list of numbers separated by commas : ")
+        inputlist = list(map(int, values.split(",")))
+    except ValueError:
+        print("Kindly enter in the values in the requested format")
+        exit()
+
+    checkList(inputlist)
